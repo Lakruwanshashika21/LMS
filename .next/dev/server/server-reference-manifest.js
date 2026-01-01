63f606a26b3a6c8cdd2afaadf9c203da9a6edb89
Lakruwanshashika21 <135962682+Lakruwanshashika21@users.noreply.github.com>
2026-01-01 17:44:51 +0530

    Clean up: Remove .next build cache from repository

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"jXlRoDOijUumL5DZoApSLjXv/9GWG0E1wbV9Du2/JuM=\"\n}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"nsDf+FUg/q0JFQsBcntgNCqCEEDaZhc1tJSrpotE2T4=\"\n}"
